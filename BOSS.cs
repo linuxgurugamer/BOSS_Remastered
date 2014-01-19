4c0baaa8afa9882423715f539c8d5242e0a7961d
synik4l <synik4l@gmail.com>
2014-01-19 00:43:08 -0600

    Added ability to change out menu skins, cleaned up some bugs, added help menu, adding my own antialiasing.

--- BOSS.cs
+++ BOSS.cs
@@ -16,34 +16,51 @@ along with The Bolt-On Screenshot System.  If not, see <http://www.gnu.org/licen
 
 using System;
 using System.ComponentModel;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Drawing.Printing;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading;
+using SyNUtils;
 using Toolbar;
 using UnityEngine;
-using KSP.IO;
+using Color = UnityEngine.Color;
+using File = KSP.IO.File;
 
 [KSPAddon(KSPAddon.Startup.EveryScene, false)]
 public class BOSS : MonoBehaviour
 {
+
+    public Vector2 scrollPosition;
     public Vessel activeVessel;
-    protected Rect helpWindowPos, BurstPos;
-    private string kspDir = KSPUtil.ApplicationRootPath + @"GameData/BOSS/PluginData/BOSS/";
+    protected Rect helpWindowPos, BurstPos, showUIPos;
+    private string BossFldr = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location)+@"\";
+    private string kspPluginDataFldr = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location)  + @"\PluginData\BOSS\";
     public int screenshotCount, superSampleValueInt = 1, burstTime = 1;
     private double burstInterval = 1;
-
     public string superSampleValueString = "1",
-                  burstTimeString = "1",
-                  burstIntervalString = "1",
-                  screenshotKey = "z",
-                  showGUIKey = "p";
+        burstTimeString = "1",
+        burstIntervalString = "1",
+        screenshotKey = "z",
+        showGUIKey = "p",
+        helpContent = "",
+        worked = "";
 
-    public bool showHelp = false, burstMode = false, showBurst = false;
+    public bool showUI = true,
+        burstMode = false,
+        showBurst = false,
+        showHelp = true,
+        unitySkin = true,
+        unitySS = true;
+        
     private IButton toolbarButton;
-    public BOSSSettings BOSSsettings = new BOSSSettings();
-
+    public BOSSSettings BOSSsettings = new BOSSSettings(); 
 
     public void Awake()
     {
-        if (!File.Exists<BOSS>(kspDir + "config.xml"))
+        if (!File.Exists<BOSS>(kspPluginDataFldr + "config.xml"))
         {
             try
             {
@@ -54,6 +71,20 @@ public class BOSS : MonoBehaviour
                 throw new AccessViolationException("Can't create settings file, please confirm directory is writeable.");
             }
         }
+        if (!File.Exists<BOSS>(BossFldr + "readme.txt"))
+        try
+        {
+            using (StreamReader sr = new StreamReader(BossFldr + "readme.txt"))
+            {
+                 helpContent = sr.ReadToEnd();
+                
+            }
+        }
+        catch (Exception e)
+        {
+            print("Could not read readme.txt");
+        }
+
         loadSettings();
         initToolbar();
         RenderingManager.AddToPostDrawQueue(60, new Callback(drawGUI));
@@ -62,57 +93,75 @@ public class BOSS : MonoBehaviour
     private void initToolbar()
     {
         toolbarButton = ToolbarManager.Instance.add("BOSS", "toolbarButton");
-        toolbarButton.TexturePath = showHelp ? "BOSS/bon" : "BOSS/boff";
+        toolbarButton.TexturePath = showUI ? "BOSS/bon" : "BOSS/boff";
         toolbarButton.ToolTip = "Toggle Bolt-On Screenshot System";
         toolbarButton.OnClick += (e) =>
-            {
-                if (showHelp) showHelp = false;
-                else if (!showHelp) showHelp = true;
-                toolbarButton.TexturePath = showHelp ? "BOSS/bon" : "BOSS/boff";
-                saveSettings();
-            };
+        {
+            if (showUI) showUI = false;
+            else if (!showUI) showUI = true;
+            toolbarButton.TexturePath = showUI ? "BOSS/bon" : "BOSS/boff";
+            saveSettings();
+        };
     }
 
     private void drawGUI()
     {
-        GUI.skin = HighLogic.Skin;
+        if (unitySkin) GUI.skin = null;
+        else GUI.skin = HighLogic.Skin;
+
+        if (showUI)
+            showUIPos = GUILayout.Window(568, showUIPos, UIContent, "B.O.S.S. Control", GUILayout.Width(150),
+                GUILayout.Height(150));
+
         if (showHelp)
-        {
-            helpWindowPos = GUILayout.Window(568, helpWindowPos, UIContent, "B.O.S.S. Control", GUILayout.Width(150),
-                                             GUILayout.Height(150));
-        }
+            helpWindowPos = GUILayout.Window(570, helpWindowPos, UIContentHelp, "Help!!!", GUILayout.Width(300),
+                GUILayout.Height(300));
+
         if (burstMode)
-        {
             BurstPos = GUILayout.Window(569, BurstPos, UIContentBurst, "Burst Control", GUILayout.Width(150),
-                                        GUILayout.Height(150));
-        }
+                GUILayout.Height(150));
     }
 
     public void Update()
     {
-        if (Input.GetKeyDown(screenshotKey))
+        if (burstMode)
         {
-            if (burstMode)
-            {
-                saveSettings();
-                loadSettings();
-                print("burst mode start");
-                fireBurstShot();
+            superSampleValueInt = 1;
+            superSampleValueString = "1";
+        }
 
-            }
-            else
+        try
+        {
+            if (Input.GetKeyDown(screenshotKey))
             {
-                saveSettings();
-                loadSettings();
-                print("Screenshot button pressed!");
-                takeScreenshot();
+                if (burstMode)
+                {
+                    saveSettings();
+                    loadSettings();
+                    print("burst mode start");
+                    fireBurstShot();
+                }
+                else
+                {
+                    saveSettings();
+                    loadSettings();
+                    print("Screenshot button pressed!");
+                    takeScreenshot();
+                }
+            }
+            if (Input.GetKeyDown(showGUIKey))
+            {
+                if (showUI) showUI = false;
+                else if (!showUI) showUI = true;
+                toolbarButton.TexturePath = showUI ? "BOSS/bon" : "BOSS/boff";
             }
         }
-        if (Input.GetKeyDown(showGUIKey))
+        catch (UnityException e)
         {
-            if (showHelp) showHelp = false;
-            else if (!showHelp) showHelp = true;
-            toolbarButton.TexturePath = showHelp ? "BOSS/bon" : "BOSS/boff";
+            if (screenshotKey != "invalid" || screenshotKey != "")
+            {
+                screenshotKey = "invalid";
+            }
         }
     }
 
@@ -121,7 +170,7 @@ public class BOSS : MonoBehaviour
         GUILayout.BeginVertical();
 
         GUILayout.Label("Set interval in secs: " + burstInterval.ToString(), GUILayout.ExpandHeight(true),
-                        GUILayout.ExpandWidth(true));
+            GUILayout.ExpandWidth(true));
         if (!double.TryParse(burstIntervalString, out burstInterval))
         {
             burstIntervalString = " ";
@@ -130,7 +179,7 @@ public class BOSS : MonoBehaviour
 
 
         GUILayout.Label("Set time in secs: " + burstTime.ToString(), GUILayout.ExpandHeight(true),
-                        GUILayout.ExpandWidth(true));
+            GUILayout.ExpandWidth(true));
         if (!int.TryParse(burstTimeString, out burstTime))
         {
             burstTimeString = " ";
@@ -139,6 +188,14 @@ public class BOSS : MonoBehaviour
 
 
         GUILayout.EndVertical();
+        GUI.DragWindow(new Rect(0, 0, 10000, 20));
+    }
+
+    private void UIContentHelp(int windowID)
+    {
+        scrollPosition = GUILayout.BeginScrollView(scrollPosition, GUILayout.Width(295), GUILayout.Height(295));
+        GUILayout.Label(helpContent);
+        GUILayout.EndScrollView();
         GUI.DragWindow(new Rect(0, 0, 10000, 20));
     }
 
@@ -153,9 +210,10 @@ public class BOSS : MonoBehaviour
         mainGUI.padding = new RectOffset(8, 8, 8, 8);
 
         GUILayout.BeginVertical();
-
         GUILayout.Label("Current supersample value: " + superSampleValueInt.ToString(), GUILayout.ExpandHeight(true),
-                        GUILayout.ExpandWidth(true));
+            GUILayout.ExpandWidth(true));
+        GUILayout.Label("Did it work?" + worked, GUILayout.ExpandHeight(true),
+           GUILayout.ExpandWidth(true));
         GUILayout.Label("Current take ss key: ", GUILayout.ExpandHeight(true), GUILayout.ExpandWidth(true));
         screenshotKey = GUILayout.TextField(screenshotKey);
         GUILayout.Label("Supersample value: ");
@@ -166,7 +224,6 @@ public class BOSS : MonoBehaviour
         }
         superSampleValueString = GUILayout.TextField(superSampleValueString);
 
-
         if (GUILayout.Button("Take Screenshot", mainGUI, GUILayout.Width(125)))
             //GUILayout.Button is "true" when clicked
         {
@@ -174,6 +231,9 @@ public class BOSS : MonoBehaviour
             {
                 saveSettings();
                 loadSettings();
+                superSampleValueInt = 1;
+                superSampleValueString = "1";
+
                 print("burst mode shot");
                 fireBurstShot();
             }
@@ -186,6 +246,13 @@ public class BOSS : MonoBehaviour
             }
         }
         burstMode = GUILayout.Toggle(burstMode, "Toggle Burst", GUILayout.ExpandWidth(true));
+        showHelp = GUILayout.Toggle(showHelp, "Toggle Help", GUILayout.ExpandWidth(true));
+        if (unitySkin) unitySkin = GUILayout.Toggle(unitySkin, "Toggle ksp skin", GUILayout.ExpandWidth(true));
+        else unitySkin = GUILayout.Toggle(unitySkin, "Toggle unity skin", GUILayout.ExpandWidth(true));
+        if (unitySS) unitySS = GUILayout.Toggle(unitySS, "Toggle synik4l processing", GUILayout.ExpandWidth(true));
+        else unitySS = GUILayout.Toggle(unitySS, "Toggle unity processing", GUILayout.ExpandWidth(true));
+
+
         GUILayout.Label(screenshotCount + " screenshots taken.");
         GUILayout.EndVertical();
         GUI.DragWindow(new Rect(0, 0, 10000, 20));
@@ -196,9 +263,43 @@ public class BOSS : MonoBehaviour
         string screenshotFilename = "SS_" + DateTime.Today.ToString("MM-dd-yyyy") + "_" +
                                     DateTime.Now.ToString("HH-mm-ss");
         print("Screenshot Count:" + screenshotCount);
-        print(kspDir + screenshotFilename + ".png");
+        print(kspPluginDataFldr + screenshotFilename + ".png");
         print("Your supersample value was " + superSampleValueInt + "!");
-        Application.CaptureScreenshot(kspDir + screenshotFilename + ".png", superSampleValueInt);
+        if (unitySS)
+            Application.CaptureScreenshot(kspPluginDataFldr + screenshotFilename + ".png", superSampleValueInt);
+        else
+        {
+           
+            print(kspPluginDataFldr + screenshotFilename);
+            BackgroundWorker bw = new BackgroundWorker();
+
+            bw.WorkerReportsProgress = true;
+
+            bw.DoWork += new DoWorkEventHandler(
+                delegate(object o, DoWorkEventArgs args)
+                {
+                    BackgroundWorker b = o as BackgroundWorker;
+                    Application.CaptureScreenshot(kspPluginDataFldr + screenshotFilename + ".png", 1);
+                    Thread.Sleep(5000);
+                });
+
+            // what to do when worker completes its task (notify the user)
+            bw.RunWorkerCompleted += new RunWorkerCompletedEventHandler(
+                delegate(object o, RunWorkerCompletedEventArgs args)
+                {
+                    showHelp = !showHelp;
+                    SyNUtils.AntiAliaser process = new AntiAliaser();
+                    worked = process.processImage(kspPluginDataFldr + screenshotFilename, superSampleValueInt);
+                    System.IO.File.Delete(kspPluginDataFldr + screenshotFilename + "2.png");
+                    print("synik4l Screenshot processing successful!");
+                });
+
+            bw.RunWorkerAsync();
+
+           
+           
+            
+        }
         screenshotCount++;
         saveSettings();
     }
@@ -207,44 +308,40 @@ public class BOSS : MonoBehaviour
     {
         int bursts = burstTime;
         int interval = Convert.ToInt32(burstInterval*1000);
-     BackgroundWorker bw = new BackgroundWorker();
+        BackgroundWorker bw = new BackgroundWorker();
 
-        // this allows our worker to report progress during work
         bw.WorkerReportsProgress = true;
-        // what to do in the background thread
+
         bw.DoWork += new DoWorkEventHandler(
-        delegate(object o, DoWorkEventArgs args)
-        {
-            BackgroundWorker b = o as BackgroundWorker;
-
-            // do some simple processing for 10 seconds
-            for (; bursts > 0; bursts--)
+            delegate(object o, DoWorkEventArgs args)
             {
-                takeScreenshot();
-                Thread.Sleep(interval);
-            }
+                BackgroundWorker b = o as BackgroundWorker;
 
-        });
+
+                for (; bursts > 0; bursts--)
+                {
+                    takeScreenshot();
+                    Thread.Sleep(interval);
+                }
+            });
 
         // what to do when worker completes its task (notify the user)
         bw.RunWorkerCompleted += new RunWorkerCompletedEventHandler(
-        delegate(object o, RunWorkerCompletedEventArgs args)
-        {
-            burstMode = !burstMode;
-        });
+            delegate(object o, RunWorkerCompletedEventArgs args) { burstMode = !burstMode; });
 
         bw.RunWorkerAsync();
     }
 
     private void createSettings()
     {
-        
         BOSSsettings.SetValue("BOSS::BurstPos.x", "250");
         BOSSsettings.SetValue("BOSS::BurstPos.y", "250");
-        BOSSsettings.SetValue("BOSS::helpWindowPos.x", "400");
-        BOSSsettings.SetValue("BOSS::helpWindowPos.y", "400");
+        BOSSsettings.SetValue("BOSS::helpWindowPos.x", "600");
+        BOSSsettings.SetValue("BOSS::helpWindowPos.y", "500");
+        BOSSsettings.SetValue("BOSS::showUIPos.x", "400");
+        BOSSsettings.SetValue("BOSS::showUIPos.y", "400");
         BOSSsettings.SetValue("BOSS::screenshotCount", "0");
-        BOSSsettings.SetValue("BOSS::showHelp", "True");
+        BOSSsettings.SetValue("BOSS::showUI", "True");
         BOSSsettings.SetValue("BOSS::screenshotKey", "z");
         BOSSsettings.SetValue("BOSS::showGUIKey", "p");
         BOSSsettings.SetValue("BOSS::supersampValue", "1");
@@ -257,13 +354,14 @@ public class BOSS : MonoBehaviour
 
     private void saveSettings()
     {
-       
         BOSSsettings.SetValue("BOSS::BurstPos.x", BurstPos.x.ToString());
         BOSSsettings.SetValue("BOSS::BurstPos.y", BurstPos.y.ToString());
         BOSSsettings.SetValue("BOSS::helpWindowPos.x", helpWindowPos.x.ToString());
         BOSSsettings.SetValue("BOSS::helpWindowPos.y", helpWindowPos.y.ToString());
+        BOSSsettings.SetValue("BOSS::showUIPos.x", showUIPos.x.ToString());
+        BOSSsettings.SetValue("BOSS::showUIPos.y", showUIPos.y.ToString());
         BOSSsettings.SetValue("BOSS::screenshotCount", screenshotCount.ToString());
-        BOSSsettings.SetValue("BOSS::showHelp", showHelp.ToString());
+        BOSSsettings.SetValue("BOSS::showUI", showUI.ToString());
         BOSSsettings.SetValue("BOSS::screenshotKey", screenshotKey);
         BOSSsettings.SetValue("BOSS::showGUIKey", showGUIKey);
         BOSSsettings.SetValue("BOSS::supersampValue", superSampleValueString);
@@ -281,8 +379,10 @@ public class BOSS : MonoBehaviour
         BurstPos.y = Convert.ToSingle(BOSSsettings.GetValue("BOSS::BurstPos.y"));
         helpWindowPos.x = Convert.ToSingle(BOSSsettings.GetValue("BOSS::helpWindowPos.x"));
         helpWindowPos.y = Convert.ToSingle(BOSSsettings.GetValue("BOSS::helpWindowPos.y"));
+        showUIPos.x = Convert.ToSingle(BOSSsettings.GetValue("BOSS::showUIPos.x"));
+        showUIPos.y = Convert.ToSingle(BOSSsettings.GetValue("BOSS::showUIPos.y"));
         screenshotCount = Convert.ToInt32(BOSSsettings.GetValue("BOSS::screenshotCount"));
-        showHelp = Convert.ToBoolean(BOSSsettings.GetValue("BOSS::showHelp"));
+        showUI = Convert.ToBoolean(BOSSsettings.GetValue("BOSS::showUI"));
         screenshotKey = (BOSSsettings.GetValue("BOSS::screenshotKey"));
         showGUIKey = (BOSSsettings.GetValue("BOSS::showGUIKey"));
         superSampleValueString = (BOSSsettings.GetValue("BOSS::supersampValue"));
--- readme.txt
+++ readme.txt
@@ -1,4 +1,23 @@
 ﻿BOSS
 Bolt On Screenshot System
 
-More info coming soon
+Important
+Using this mod requires alot of ram. Thus burst fire mode is restricted to no supersampling. Although you can still use supersampling when not using burst fire.
+I recommend not going higher than 3 or 4.
+
+Description
+This plugin allows you to take screenshots within KSP at a higher resolution than your current screen resolution, using a technique called 'supersampling'. 
+Press 'z' to take a screenshot(default) and 'p' to hide the UI. 
+
+Changelog
+v2.1.2 - Fixed up the issues with hiding UI, made it persistent and changed it to the p key.
+v2.1.1 - Fixed an issue in the paths given to the screenshot method. Screenshots should actually save now...
+v2.1 - Added in the ability to hide the BOSS UI with the F2 key.
+v2.0 - Updated for 0.23 compatibility. Refactored and removed a lot of code; mainly to do with requiring a part and then method of text field input.
+v0.2.3 - Updated for 0.21 compatibility.
+v0.2.2 - Fixed issues with multiple Bolts being on the same craft and thus causing BOSS to bug out.
+v0.2.1 - Reverted to old internal structure for GUI, will progress with it at another date.
+v0.2 - Changed to PartModule. Updated for 0.18. Changed GUI. Fixed various little bugs.
+v0.1.2 - Prevented spamming of the debug log. Fixed non-updating text field.
+v0.1.1 - Added in Settings Persistence.
+v0.1 - Public Release
